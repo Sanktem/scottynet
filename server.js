2cdb3e92fb2f4d73df5aac5e334386cffc93d2fe
Sanktem <dale@scottynet.com>
2025-06-06 13:41:32 -0500

    everything all at once

---
+++ server.js
@@ -0,0 +1,37 @@
+//how to run the server
+/* 
+
+npm install express
+node server.js
+
+*/
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const COUNTER_FILE = path.join(__dirname, 'counter.txt');
+
+app.use(express.static(__dirname));
+
+// Get current count
+app.get('/api/count', (req, res) => {
+    fs.readFile(COUNTER_FILE, 'utf8', (err, data) => {
+        if (err) return res.json({ count: 0 });
+        res.json({ count: parseInt(data, 10) || 0 });
+    });
+});
+
+// Increment count
+app.post('/api/increment', (req, res) => {
+    fs.readFile(COUNTER_FILE, 'utf8', (err, data) => {
+        let count = parseInt(data, 10) || 0;
+        count++;
+        fs.writeFile(COUNTER_FILE, count.toString(), () => {
+            res.json({ count });
+        });
+    });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
